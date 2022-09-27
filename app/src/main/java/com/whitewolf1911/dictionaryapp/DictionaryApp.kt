51b323818a12c2cd48890ac725abce295a710b16
Ali Başoğlu <alibasoglu1911@gmail.com>
2022-09-27 18:03:52 +0300

    Implement DI modules for hilt

---
+++ app/src/main/java/com/whitewolf1911/dictionaryapp/DictionaryApp.kt
@@ -0,0 +1,7 @@
+package com.whitewolf1911.dictionaryapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DictionaryApp : Application()
---
+++ app/src/main/java/com/whitewolf1911/dictionaryapp/dictionary/di/WordInfoModule.kt
@@ -0,0 +1,65 @@
+package com.whitewolf1911.dictionaryapp.dictionary.di
+
+import android.app.Application
+import androidx.room.Room
+import com.google.gson.Gson
+import com.whitewolf1911.dictionaryapp.DictionaryApp
+import com.whitewolf1911.dictionaryapp.dictionary.data.local.WordInfoDao
+import com.whitewolf1911.dictionaryapp.dictionary.data.local.WordInfoDatabase
+import com.whitewolf1911.dictionaryapp.dictionary.data.remote.DictionaryApi
+import com.whitewolf1911.dictionaryapp.dictionary.data.repository.WordInfoRepositoryImpl
+import com.whitewolf1911.dictionaryapp.dictionary.data.util.GsonParser
+import com.whitewolf1911.dictionaryapp.dictionary.domain.repository.WordInfoRepository
+import com.whitewolf1911.dictionaryapp.dictionary.domain.usecase.GetWordInfo
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+@Module
+@InstallIn(SingletonComponent::class)
+object WordInfoModule {
+
+    @Provides
+    @Singleton
+    fun provideGetWordInfoUseCase(
+        repository: WordInfoRepository
+    ): GetWordInfo {
+        return GetWordInfo(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWordInfoRepository(
+        api: DictionaryApi,
+        db: WordInfoDatabase
+    ): WordInfoRepository {
+        return WordInfoRepositoryImpl(dao = db.dao, api = api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWordInfoDatabase(app: Application): WordInfoDatabase {
+        return Room.databaseBuilder(
+            app,
+            WordInfoDatabase::class.java,
+            "word_db"
+        )
+            .addTypeConverter(GsonParser(Gson()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDictionaryApi(): DictionaryApi {
+        return Retrofit.Builder()
+            .baseUrl(DictionaryApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(DictionaryApi::class.java)
+    }
+}
